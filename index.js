e2a9c3033c27e55d5bc7427242743dc2ac0e7d51
Natalisp <nathalipat@gmail.com>
2016-11-29 11:04:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time:  Math.round(Date.now()/1000),
+  startTicking: function() {
+    setInterval(() => {
+        this.time ++;
+    }, 1000)
+}
+}
+
+
+function censor(word, sentence) {
+  const replacement = new RegExp(word, 'g');
+  return sentence.replace(replacement, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');
